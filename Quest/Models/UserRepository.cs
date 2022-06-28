1eb06c27d833f4e2c25f24ff99935d994e34435c
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-28 20:54:19 -0300

    Craindo as interface dos repositorios.

---
+++ Quest/Models/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Quest.Models
+{
+	interface UserRepository
+	{
+		List<User> User { get; }
+		User this[int id] { get; }
+		User NewUser(User user);
+		User UpdateUser(User user);
+		void DeleteUser(int id);
+
+	}
+}
