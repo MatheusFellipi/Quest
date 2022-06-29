df116d86a801e4103e82f3bbabc6a9349f9b67e7
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-28 21:45:12 -0300

    Craindo a funcao de gerar token.

---
+++ Quest/Services/TokenServices.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
+using Quest.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Quest.Services
+{
+	public class TokenServices
+	{
+		public static string GenerateToken(User user)
+		{
+			var tokenHandle = new JwtSecurityTokenHandler();
+			var key = Encoding.ASCII.GetBytes(Settings.Secret);
+			var tokenDescription = new SecurityTokenDescriptor
+			{
+				 Subject = new ClaimsIdentity(new Claim[] { 
+					new Claim(ClaimTypes.Email,user.Email.ToString()),
+					new Claim(ClaimTypes.Role,user.Role.ToString())
+				 }),
+				 Expires = DateTime.UtcNow.AddHours(2),
+				 SigningCredentials =
+				 new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature)
+			};
+			var token = tokenHandle.CreateToken(tokenDescription);
+
+			return tokenHandle.WriteToken(token);
+		}
+	}
+}
