ac159aa80391c6198cc952d3fc0cacb3126b70ac
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 16:39:10 -0300

    Criando a listagem de use casa

---
+++ Quest/Module/User/UseCase/ListUserUseCase/ListUserUseCase.cs
@@ -0,0 +1,23 @@
+﻿using Quest.Entities;
+using Quest.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quest.UseCase
+{
+	public class ListUserUseCase
+	{
+		private readonly IUserRepository _repository;
+		public ListUserUseCase(IUserRepository repo)
+		{
+			_repository = repo;
+		}
+
+		public async Task<User> GetByEmail(string email)
+		{
+			return await _repository.GetByEmail(email);
+		}
+	}
+}
