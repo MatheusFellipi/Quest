8de6f801ca80958bfbb692e01fbe848c379d9181
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-28 23:04:32 -0300

    Limpando a pasta deixando so os models.

--- Quest/Models/Post.cs
+++ Quest/Models/Post.cs
@@ -7,7 +7,6 @@ namespace Quest.Models
 	{
 		[Key]
 		public int Id { get; set; }
-
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public bool Active { get; set; }
--- Quest/Models/User.cs
+++ Quest/Models/User.cs
@@ -9,6 +9,8 @@ namespace Quest.Models
 		public int Id { get; set; }
 		public string Email { get; set; }
 		public string Password { get; set; }
+		public string Role { get; set; }
 
 	}
+	
 }
