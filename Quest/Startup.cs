ed8ed399ad035e37d4129d205cf51a6d861a2799
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 00:22:10 -0300

    Alterando o namespapce.

--- Quest/Startup.cs
+++ Quest/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Quest.Data;
-using Quest.Models;
 using Quest.Repositories;
 using System.Text;
 
