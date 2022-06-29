d5ae3dcac8aefc5131149d181492fa21bc4dd39e
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 14:23:18 -0300

    Criando o uso case de criar um post.

---
+++ Quest/Module/Post/UseCase/CreateUseCase/PostCreateUseCase.cs
@@ -0,0 +1,24 @@
+﻿using Quest.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Quest.UseCase
+{
+	public class PostCreateUseCase
+	{
+		private readonly IPostIRepository _repository;
+		public PostCreateUseCase(IPostIRepository repo)
+		{
+			_repository = repo;
+		}
+
+		public async Task<dynamic> NewPost(Post post)
+		{
+			if (string.IsNullOrWhiteSpace(post.Description) || string.IsNullOrWhiteSpace(post.Title))
+				throw new Exception("preencher o dados");
+
+			return await _repository.NewPost(post);
+		}
+
+	}
+}
