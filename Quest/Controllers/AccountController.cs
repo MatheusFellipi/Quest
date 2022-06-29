a21f285200e900c1bcfe53cd5ac9cc6a47f47db2
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 11:37:08 -0300

    Craindo a funcao para login.

--- Quest/Controllers/AccountController.cs
+++ Quest/Controllers/AccountController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Quest.Data;
 using Quest.Entities;
+using Quest.Repositories;
+using Quest.Services;
 
 namespace Quest.Controllers
 {
@@ -15,12 +17,30 @@ namespace Quest.Controllers
 	[ApiController]
 	public class AccountController : ControllerBase
 	{
+		private readonly IUserRepository _repository;
+		public AccountController(IUserRepository repo)
+		{
+			_repository = repo;
+		}
+
 		[HttpPost]
 		[Route("login")]
 		[AllowAnonymous]
-
-		public async void /*Task<ActionResult<dynamic>>*/ Autheticate([FromBody] User model)
+		public async Task<dynamic> Autheticate([FromBody] User model)
 		{
+			
+			var user = await _repository.GetByEmail(model.Email);
+
+			if (user == null)
+				return NotFound(new { message = "Usuario ou senha invalido" });
+
+			var token = TokenServices.GenerateToken(user);
+			user.Password = "";
+			return new
+			{
+				user = user,
+				token = token
+			};
 		}
 
 	}
