767330d3f18e887531627ccf064f38d412a75c6a
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 17:44:17 -0300

    Craindo o caso de uso para deletar.

---
+++ Quest/Module/Post/UseCase/PostDeleteUseCase/PostDeleteUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Quest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quest.UseCase
+{
+	public class PostDeleteUseCase
+	{
+		private readonly IPostIRepository _repository;
+		public PostDeleteUseCase(IPostIRepository repo)
+		{
+			_repository = repo;
+		}
+
+		public async Task<dynamic> Delete(int id)
+		{
+			var post = await _repository.GetById(id);
+
+			if (post != null)
+			{
+				post.IsDelete = true;
+			}
+
+			return await _repository.GetPost();
+		}
+	}
+}
