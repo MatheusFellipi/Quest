c94c72755da8ac299332c997e27ee9374a6481d3
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 14:48:05 -0300

    Criando a lista de post.

---
+++ Quest/Module/Post/UseCase/ListUseCase/ListUseCase.cs
@@ -0,0 +1,27 @@
+﻿using Quest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quest.UseCase
+{
+	public class ListUseCase
+	{
+		private readonly IPostIRepository _repository;
+		public ListUseCase(IPostIRepository repo)
+		{
+			_repository = repo;
+		}
+
+		public async Task<IEnumerable<Post>> Get()
+		{
+			return await _repository.GetPost();
+		}
+
+		public async Task<IEnumerable<Post>> GetByUser(int id)
+		{
+			return await _repository.GetByUser(id);
+		}
+	}
+}
