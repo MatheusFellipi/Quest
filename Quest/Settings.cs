c9504f024b96fd47ea7415ef95cb50d49ccb4be6
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-28 21:46:02 -0300

    Configurando o uso de autenticação.

---
+++ Quest/Settings.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace Quest
+{
+	public static class Settings
+	{
+		public static string Secret = "feersdsdawdsgggtsdawds55541a2wdas1d1awda1351153";
+	}
+}
--- Quest/Startup.cs
+++ Quest/Startup.cs
@@ -1,11 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Quest.Data;
-
+using System.Text;
 
 namespace Quest
 {
@@ -21,9 +23,27 @@ namespace Quest
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddCors();
 			services.AddDbContext<DataContext> (opt => opt.UseInMemoryDatabase("database"));
 			services.AddScoped<DataContext, DataContext>();
 			services.AddControllers();
+
+			var key = Encoding.ASCII.GetBytes(Settings.Secret);
+			services.AddAuthentication(x =>
+			{
+				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(x => {
+				x.RequireHttpsMetadata = false;
+				x.SaveToken = true;
+				x.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidateAudience = false,
+					ValidateIssuer= false
+				};
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +56,9 @@ namespace Quest
 
 			app.UseRouting();
 
+			app.UseCors(x => x.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
+
+			app.UseAuthentication();
 			app.UseAuthorization();
 
 			app.UseEndpoints(endpoints =>
