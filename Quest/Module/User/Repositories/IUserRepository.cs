15ebf2c69a0ba17b301183945936a6cf770a2847
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 11:35:27 -0300

    Pesquisando por email.

--- Quest/Module/User/Repositories/IUserRepository.cs
+++ Quest/Module/User/Repositories/IUserRepository.cs
@@ -8,7 +8,8 @@ namespace Quest.Repositories
 	public interface IUserRepository 
 	{
 		Task<IEnumerable<User>> GetUser();
-		Task<User> GetById(int id); 
+		Task<User> GetById(int id);
+		Task<User> GetByEmail(string id);
 		Task<User> NewUser(User user);
 		User UpdateUser(User user);
 		void DeleteUser(int id);
--- Quest/Module/User/Repositories/UserRepository.cs
+++ Quest/Module/User/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using Quest.Data;
 using Quest.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Quest.Repositories
@@ -17,6 +18,8 @@ namespace Quest.Repositories
 
 		public async Task<User> GetById(int id) => await _repositorio.Users.FindAsync(id);
 
+		public async Task<User> GetByEmail(string email) => await _repositorio.Users.AsNoTracking()
+				.Where(x => x.Email == email).FirstOrDefaultAsync();
 		public async Task<IEnumerable<User>> GetUser()
 		{
 			return await _repositorio.Users.ToListAsync();
