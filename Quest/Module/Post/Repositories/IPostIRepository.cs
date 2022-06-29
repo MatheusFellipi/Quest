140a9f0a2c2adc65d1137e703eaf6dd64cc184f5
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-29 10:52:19 -0300

    Criando o repositorio do post.

--- Quest/Module/Post/Repositories/IPostIRepository.cs
+++ Quest/Module/Post/Repositories/IPostIRepository.cs
@@ -1,7 +1,16 @@
 ﻿
+using Quest.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace Quest
 {
 	public interface IPostIRepository
 	{
+		Task<IEnumerable<Post>> GetPost();
+		Task<List<Post>> GetByUser(int id);
+		Task<Post> NewPost(Post user);
+		Post UpdatePost(Post user);
+		void DeletePost(int id);
 	}
 }
---
+++ Quest/Module/Post/Repositories/PostIRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Quest.Data;
+using Quest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quest.Repositories
+{
+	public class PostIRepository : IPostIRepository
+	{
+
+		private readonly DataContext _repositorio;
+		public PostIRepository(DataContext repositorio)
+		{
+			_repositorio = repositorio;
+		}
+
+		public void DeletePost(int id)
+		{
+			_repositorio.Remove(id);
+		}
+
+		public async Task<List<Post>> GetByUser(int id)
+		{
+			return await _repositorio.Posts
+				.AsNoTracking()
+				.Where(x => x.Id_User == id)
+				.ToListAsync(); 
+		}
+
+		public async Task<IEnumerable<Post>> GetPost()
+		{
+			return await _repositorio.Posts.ToListAsync();
+		}
+
+		public async Task<Post> NewPost(Post post)
+		{
+			if (post.Id == 0)
+			{
+				_repositorio.Posts.Add(post);
+				await _repositorio.SaveChangesAsync();
+				return post;
+			}
+			return post;
+		}
+
+		public Post UpdatePost(Post user)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
